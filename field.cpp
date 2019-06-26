4f6cccf5faab6858b25a99c92cc1f77a4966a8b8
Freeman <egor.kovalev616@gmail.com>
2019-06-26 21:59:30 +0300

    update field class

--- field.cpp
+++ field.cpp
@@ -1,1 +1,51 @@
 #include "field.h"
+
+field::field()
+{
+	un_height = 30;
+	un_width = 40;
+	un_areaSize = 10;
+	for (unsigned int i = 0; i < un_height; i++)
+	{
+		for (unsigned int j = 0; j < un_width; j++)
+		{
+			cell nextCell(i, j);
+			field_cells.push_back(nextCell);
+		}
+	}
+}
+
+field::field(const unsigned int newHeight, const unsigned int newWidht)
+{
+	un_height = newHeight;
+	un_width = newWidht;
+	un_areaSize = 10;
+	for (unsigned int i = 0; i < un_height; i++)
+	{
+		for (unsigned int j = 0; j < un_width; j++)
+		{
+			cell nextCell(i, j);
+			field_cells.push_back(nextCell);
+		}
+	}
+}
+
+field::field(const unsigned int newHeight, const unsigned int newWidht, const unsigned int newAreaSize)
+{
+	un_height = newHeight;
+	un_width = newWidht;
+	un_areaSize = newAreaSize;
+	for (unsigned int i = 0; i < un_height; i++)
+	{
+		for (unsigned int j = 0; j < un_width; j++)
+		{
+			cell nextCell(i, j);
+			field_cells.push_back(nextCell);
+		}
+	}
+}
+
+field::~field()
+{
+	field_cells.clear();
+}
--- field.h
+++ field.h
@@ -1,8 +1,18 @@
 #pragma once
+#include <vector>
+#include "cell.h"
+
 class field
 {
 public:
+	field(); // Конструктор по умолчанию;
+	field(const unsigned int, const unsigned int); // Создаем с кастомными шириной и высотой, площадь клетки по умолчанию;
+	field(const unsigned int, const unsigned int, const unsigned int); // Создаем со всеми кастомными параметрами;
+	~field(); // Деструктор по умолчанию;
 protected:
+	unsigned int un_height, un_width, un_areaSize; 
+	// Высота и ширина поля, а так же размер ячейки (она квадратная, так что достаточно указать только сторону)
+	std::vector<cell> field_cells; //Собственно контейнер с ячейками;
 private:
 
 };
